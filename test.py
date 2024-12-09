1caf07f265d80e5f332d43f61fd541fa6c18ef4c
andathan <and13ath@gmail.com>
2024-12-09 17:06:35 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+import sys
+import scipy
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import math
+import mpmath as mp
+from datetime import *
+import sys
+import pandas as pd
+import itertools
+from matplotlib import rc
+from matplotlib.ticker import MaxNLocator
+import matplotlib as mpl
+
+print("All libraries seem to be imported, you can go ahead and run the experiments!")
